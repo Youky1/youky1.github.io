787fba41db4754d059be495ca1376d41e2fe5ab1
Youky <1911303982@qq.com>
2023-03-22 23:55:34 +0800

    feat: 重构主页UI

--- docs/.vuepress/components/Index.vue
+++ docs/.vuepress/components/Index.vue
@@ -1,27 +1,33 @@
 <template>
-  <div>
-    <main>
-      <div id="slogan">
-        <p class="bigSlogan">{{ slogan.big }}</p>
-        <p class="littleSlogan">{{ slogan.little }}</p>
+  <div id="bg" :style="bgColor">
+    <a
+      href="https://github.com/Youky1/Youky1.github.io"
+      target="__blank"
+      class="github"
+    >
+      <img
+        src="https://cdn.staticaly.com/gh/Youky1/picture_bed@master/github.4wprxhzfbgg0.webp"
+      />
+    </a>
+    <img src="../public/author.jpg" class="avatar" />
+    <div class="info">
+      <div v-for="(item, index) in infos" :key="item.text" style="display:flex">
+        <div v-if="index !== 0" class="line"></div>
+        <a :href="item.link" class="center">
+          <img :src="item.icon" class="icon" />{{ item.text }}
+        </a>
       </div>
-      <div id="btnContainer">
-        <button
-          round
-          type="primary"
-          style="margin-right: 10vw"
-          class="size"
-          @click="goBlog"
-        >
-          <span class="iconfont iconboke"></span>
-          Blog
-        </button>
-        <button round type="primary" class="size" @click="goCV">
-          <span class="iconfont iconjianli1"></span>
-          CV
-        </button>
-      </div>
-    </main>
+    </div>
+    <footer>
+      <a
+        v-for="item in links"
+        :key="item.text"
+        :href="item.url"
+        class="center sites"
+        target="__blank"
+        >{{ item.text }}</a
+      >
+    </footer>
   </div>
 </template>
 
@@ -29,94 +35,96 @@
 export default {
   data() {
     return {
-      slogan: {
-        big: "諦めない人は何も変えられない",
-        little: "什么都无法放弃的人，什么都改变不了",
-      },
+      infos: [
+        {
+          text: "博客",
+          link: "/article",
+          icon:
+            "https://cdn.staticaly.com/gh/Youky1/picture_bed@master/blog.1tj45u2jz2dc.webp"
+        },
+        {
+          text: "Timeline",
+          link: "/timeline",
+          icon:
+            "https://cdn.staticaly.com/gh/Youky1/picture_bed@master/timeline.5359j2vsle00.webp"
+        },
+        {
+          text: "分类",
+          link: "/category",
+          icon:
+            "https://cdn.staticaly.com/gh/Youky1/picture_bed@master/category.1x5z2nsae8sg.webp"
+        },
+        {
+          text: "Tag",
+          link: "/tag",
+          icon:
+            "https://cdn.staticaly.com/gh/Youky1/picture_bed@master/tag.69lx28acdoo0.webp"
+        }
+      ],
+      links: [
+        { text: "GitHub", url: "https://github.com/Youky1" },
+        { text: "知乎", url: "https://www.zhihu.com/people/feng-yong-qi-76" },
+        { text: "掘金", url: "https://juejin.cn/user/3139860942306360" }
+      ]
     };
   },
-  methods: {
-    goBlog() {
-      window.location.href += "article/";
-    },
-    goCV() {
-      window.location.href += "CV";
-    },
-  },
+  computed: {
+    bgColor() {
+      return {
+        background: `linear-gradient(to left bottom, hsl(${Math.floor(
+          255 * Math.random()
+        )}, 100%, 85%) 0%,hsl(${Math.floor(
+          255 * Math.random()
+        )}, 100%, 85%) 100%)`
+      };
+    }
+  }
 };
 </script>
 
 <style lang="stylus" scoped>
-    main
-        width: 100%
-        height 100vh
-        overflow-x: hidden
-        p
-            color #fff
-// PC端
-@media screen and (min-width:640px)
-    main
-        background-image url('../public/bg.jpg')
-    #slogan
-        height 60vh
-        padding-top 10vh
-        display flex
-        justify-content center
-        align-items center
-        flex-direction column
-        .bigSlogan
-            font-size 80px
-            margin-bottom 50px
-        .littleSlogan
-            font-size 40px
-            margin 50px
-    #btnContainer
-        height 20vh
-        width 100vw
-        display flex
-        justify-content center
-        align-items center
-        button
-            height 60px
-            width 140px
-            background-color #409EFF
-            color #fff
-            border-radius 10px
-            font-size 18px
-            font-family Tahoma
+#bg
+  height: 100vh
+  display: flex
+  align-items: center
+  flex-direction: column
+  .github
+    position absolute
+    top 0
+    right 0
+    img
+      width: 40px
+      height 40px
+      &:hover
+        cursor pointer
 
-@media screen and (max-width:640px)
-    main
-        background-image url('../public/bg_phone.jpg')
-        height: 100vh
-    #slogan
-        height 60vh
-        padding-top 10vh
-        display flex
-        justify-content space-around
-        align-items center
-        flex-direction column
-        .bigSlogan
-            font-size 24px
-            margin-bottom 50px
-            word-wrap break-word/*英文的时候需要加上这句，自动换行*/
-            word-break break-all
-        .littleSlogan
-            font-size 20px
-            margin 50px
-            width 200px
-    #btnContainer
-        height 20vh
-        width 100vw
-        display flex
-        justify-content center
-        align-items center
-        button
-            height 60px
-            width 140px
-            background-color #409EFF
-            color #fff
-            border-radius 10px
-            font-size 18px
-            font-family Tahoma
+  .avatar
+    margin-top 200px
+    width: 140px
+    height 140px
+    border-radius: 70px
+  .title
+    font-size 50px
+  .info
+    display: flex
+    margin-top: 40px
+    .line
+      margin 0 20px
+  .center
+    display: flex
+    align-items: center
+  .icon
+    height 20px
+    width 20px
+    margin-right 4px
+  footer
+    position absolute
+    bottom: 4vh
+    display: flex
+    .sites
+      color: #fff
+      margin: 0 10px
+      text-align: center
+      &:hover
+        color #777
 </style>
